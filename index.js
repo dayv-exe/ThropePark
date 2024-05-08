a4750de00f3026287f8a65b32f6ad6dd506ddb2b
“david <davzy007@gmail.com>
2024-05-08 07:49:45 +0100

    completed home page

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ app.get('/', (req, res) => {
   res.render('index', {title: 'Home Page'});
 });
 
+// !!!ENABLE BEFORE SUBMISSION!!!
+// app.listen(5000, () => console.log('Server started!'));
+
+// !!!DISABLE BEFORE SUBMISSION!!!
 app.listen(8000, () => console.log('Server started!'));
