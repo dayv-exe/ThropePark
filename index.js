ffdc412abf89c3bcdd5f304b74b247cadaca5e49
“david <davzy007@gmail.com>
2024-05-10 11:09:11 +0100

    sql safe

--- index.js
+++ index.js
@@ -1,47 +1,81 @@
 const express = require('express');
 const app = express();
+const sqlite3 = require('sqlite3').verbose();
+app.use(express.urlencoded({ extended: true }));
+
+const db = new sqlite3.Database("database.db", err => {
+  if (err) {
+    console.log(err.message);
+    return;
+  }
+  console.log("Connected to the db!");
+});
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.render('index', {title: 'Home Page'});
+  res.render('index', { title: 'Home Page' });
 });
 
 app.get('/areas', (req, res) => {
-  res.render('areas', {title: 'Areas'});
+  res.render('areas', { title: 'Areas', addDb: false });
 });
 app.get('/areas/:areaName', (req, res) => {
-  res.render('areapage', {title: `${req.params.areaName}`});
+  res.render('areapage', { title: `${req.params.areaName}` });
 });
 
 app.get('/rides', (req, res) => {
-  res.render('rides', {title: 'Rides'});
+  res.render('rides', { title: 'Rides' });
 });
 app.get('/rides/:rideName', (req, res) => {
-  res.render('ridepage', {title: `${req.params.rideName}`});
+  res.render('ridepage', { title: `${req.params.rideName}` });
 });
 
 app.get('/contact', (req, res) => {
-  res.render('contact', {title: 'Contact Us'});
+  res.render('contact', { title: 'Contact Us' });
 });
 
 app.get('/faqs', (req, res) => {
-  res.render('faqs', {title: 'FAQs'});
+  res.render('faqs', { title: 'FAQs' });
 });
 
 app.get('/events', (req, res) => {
-  res.render('events', {title: 'Events'});
+  res.render('events', { title: 'Events' });
 });
 app.get('/events/:eventname', (req, res) => {
-  res.render('anevent', {title: `${req.params.eventname}`});
+  res.render('anevent', { title: `${req.params.eventname}` });
 });
 
 app.get('/activity', (req, res) => {
-  res.render('activity', {title: 'Activity'});
+  res.render('activity', { title: 'Activity' });
 });
 
 app.get('/create', (req, res) => {
-  res.render('create', {title: 'Create'});
+  res.render('create', { title: 'Create' });
+});
+
+app.post("/sendData", (req, res) => {
+  db.run(req.body.sql, req.body.params, err => {
+    if (err) {
+      console.error('Error inserting data:', err);
+      res.status(500).send('Error inserting data' + err);
+    } else {
+      res.json({message: "submit success!"});
+    }
+  });
+});
+
+app.post('/getData', (req, res) => {
+  db.all(req.body.sql, [], (err, rows) => {
+      if (err) {
+          console.error('Error retrieving data:', err);
+          res.status(500).json({ error: 'Error retrieving data' });
+      } else {
+          res.json(rows);
+      }
+  });
 });
 
 // !!!ENABLE BEFORE SUBMISSION!!!
---
+++ public/requester.js
@@ -0,0 +1,72 @@
+function getFromDB(sql, renderFunc, nothingToRenderFunc, errorRenderFunc) {
+  fetch('/getData', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ sql })
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.length < 1) {
+        nothingToRenderFunc();
+      } else {
+        data.forEach(row => {
+          renderFunc(row);
+        });
+      }
+    })
+    .catch(error => {
+      errorRenderFunc(error);
+    });
+}
+
+function sendToDB(sql, params, renderFunc, errorRenderFunc) {
+
+  fetch('/sendData', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ sql, params })
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      renderFunc(data);
+    })
+    .catch(error => {
+      errorRenderFunc(error);
+    });
+}
+
+function show(htmlText, wrapperElement, cardClasses = []) {
+  const wrapper = wrapperElement
+  const card = document.createElement('div');
+  cardClasses.forEach(className => {
+    card.classList.add(className);
+  });
+
+  card.innerHTML = htmlText;
+  wrapper.appendChild(card);
+}
+
+function showOnTop(htmlText, wrapperElement, cardClasses = []) {
+  const wrapper = wrapperElement
+  const card = document.createElement('div');
+  cardClasses.forEach(className => {
+    card.classList.add(className);
+  });
+
+  card.innerHTML = htmlText;
+  wrapper.prepend(card);
+}
